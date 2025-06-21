43f5bd2158a6ab16dc1dd45d1294174ebdc576d7
Manuel de Deus <manuel.deus@wiconnect.co>
2025-06-21 19:59:28 +0100

    versao nova

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
